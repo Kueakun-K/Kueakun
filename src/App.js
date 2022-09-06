43689ac0aaaf5391f7cfe1bb92acb5210f5bc92a
BLACK <blackkitten010@gmail.com>
2022-09-07 01:51:09 +0700

    test tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+
+
+const Home = () => {
+    return(
+        <div>
+            <div className="flex">
+                <div className="flex bg-red-400 w-1/4 h-screen">
+                    <h1>test red bg</h1>
+                </div>
+                <div className="flex bg-blue-400 w-3/4 h-screen">
+                    <h2>test blue bg</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
