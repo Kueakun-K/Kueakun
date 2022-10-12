433eb047d85405554714edba9cf1bcc6fe09571d
BLACK <blackkitten010@gmail.com>
2022-10-12 17:40:14 +0700

    test navigation

--- src/pages/todo.jsx
+++ src/pages/todo.jsx
@@ -1,7 +1,25 @@
 const Todo = () => {
-    
+    let agent = navigator.userAgent;
+    // let test = navigator.storage;
+    console.log(navigator.connection);
+    const memory = navigator.deviceMemory
+    console.log (`This device has at least ${memory}GiB of RAM.`)
+    console.log('Concurrency', navigator.hardwareConcurrency)
+    console.log(navigator.hid)
+    console.log(navigator.keyboard)
+    console.log(navigator.language)
+    console.log(navigator.languages)
+    console.log(navigator.locks)
+    console.log(navigator.serial.grtPorts)
+    console.log(navigator.storage)
+    console.log(window.name)
+    console.log(navigator.userAgentData.brands);
+
     return(
-        <div>test todo</div>
+        <div className="text-white">
+            {agent}
+            {/* {test} */}
+        </div>
     )
 }
 
