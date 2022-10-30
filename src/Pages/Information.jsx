64aad1d1b051475fd116a477209b47b2b9463358
BLACK <blackkitten010@gmail.com>
2022-10-30 20:12:32 +0700

    change header in window mode to static position

--- src/Pages/Information.jsx
+++ src/Pages/Information.jsx
@@ -19,7 +19,7 @@ const Information = () => {
     <div>
       {/* header */}
 
-      <div className="flex fixed  top-0 bg-gray-100 dark:bg-slate-900 w-full z-10">
+      <div className="flex fixed sm:static  top-0 bg-gray-100 dark:bg-slate-900 w-full z-10">
         <div className=" text-red-500 dark:text-yellow-500 my-4 mx-5 ">
           <FontAwesomeIcon icon={faArrowLeft} size="lg" />
         </div>
@@ -32,12 +32,12 @@ const Information = () => {
       {/* header */}
 
       {/* img + contact*/}
-      <div className="relative h-80">
+      <div className="relative h-80 sm:h-64">
         {/* img cover */}
         <img
           src={Nebula}
           alt="Nebula"
-          className="absolute z-1 mt-14 h-48 w-full "
+          className="absolute z-1 mt-14 sm:mt-0 h-48 w-full "
         />
         {/* img cover */}
 
@@ -45,12 +45,12 @@ const Information = () => {
         <img
           src={Cat}
           alt="cat"
-          className="absolute z-[2] rounded-full w-[7.5rem] top-48 left-6 border-gray-100 dark:border-slate-900 border-4"
+          className="absolute z-[2] rounded-full w-[7.5rem] top-48 sm:top-32 left-6 border-gray-100 dark:border-slate-900 border-4"
         />
         {/* profile img */}
 
         {/* contact */}
-        <div className="absolute flex top-[16.5rem] right-12 z-[3] space-x-3 dark:text-gray-100">
+        <div className="absolute flex top-[16.5rem] sm:top-52 right-12 z-[3] space-x-3 dark:text-gray-100">
           <a
             href="https://github.com/Kueakun-K"
             target="_blank"
