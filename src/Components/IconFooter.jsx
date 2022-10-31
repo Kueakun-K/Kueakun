449c68c73469b7633cbbda19039a9ff79d9f7bbd
BLACK <blackkitten010@gmail.com>
2022-10-31 23:35:56 +0700

    change hover iconfooter

--- src/Components/IconFooter.jsx
+++ src/Components/IconFooter.jsx
@@ -10,20 +10,28 @@ import {
 const IconFooter = () => {
   return (
     <div className="flex mx-5 ">
-      <div className="flex w-1/4 justify-center items-center">
+      <div className="flex w-1/4 justify-center items-center ">
+        <div className="hover:text-red-500 dark:hover:text-yellow-500 cursor-pointer">
         <FontAwesomeIcon icon={faComment} />
         <span>&nbsp;999</span>
+        </div>
       </div>
       <div className="flex w-1/4 justify-center items-center">
+      <div className="hover:text-green-500 cursor-pointer">
         <FontAwesomeIcon icon={faRetweet} />
         <span>&nbsp;999</span>
+        </div>
       </div>
       <div className="flex w-1/4 justify-center items-center">
+      <div className="hover:text-pink-600 cursor-pointer">
         <FontAwesomeIcon icon={faHeart} />
         <span>&nbsp;999</span>
+        </div>
       </div>
       <div className="flex w-1/4 justify-center items-center">
+      <div className="hover:text-red-500 dark:hover:text-yellow-500 cursor-pointer">
         <FontAwesomeIcon icon={faShareAlt} />
+        </div>
       </div>
     </div>
   );
