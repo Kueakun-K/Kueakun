0102456e5ded67b73ea253d3d394620b64ea5046
BLACK <blackkitten010@gmail.com>
2023-02-06 15:55:13 +0700

    axios then catch

--- src/Pages/Message.jsx
+++ src/Pages/Message.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 import ProfileHeader from "../Components/ProfileHeader";
 import IconFooter from "../Components/IconFooter";
 
 const Message = () => {
   const navigate = useNavigate();
-  const [token, setToken] = React.useState(
+  const [tokenR, setTokenR] = React.useState(
     localStorage.getItem("refreshToken")
+  );
+  const [tokenA, setTokenA] = React.useState(
+    localStorage.getItem("accessToken")
   );
   const [message, setMessage] = React.useState([]);
 
   React.useEffect(() => {
-    if (!token) {
+    if (!tokenR) {
       navigate("/login");
     } else {
       axios
@@ -22,15 +26,46 @@ const Message = () => {
           {},
           {
             headers: {
-              Authorization: `Bearer ${token}`,
+              Authorization: `Bearer ${tokenA}`,
             },
           }
         )
         .then((response) => {
           setMessage(response.data);
+        })
+        .catch(() => {
+          axios
+            .get(
+              "http://localhost:3006/api/auth/token",
+              {},
+              {
+                headers: {
+                  Authorization: `Bearer ${tokenR}`,
+                },
+              }
+            )
+            .then((res) => {
+              setTokenA(res.data.accessToken)
+              axios.get(
+                "http://localhost:3006/api/user/messages",
+                {},
+                {
+                  headers: {
+                    Authorization: `Bearer ${tokenA}`,
+                  },
+                }
+              ).then((res2) => {
+                setMessage(res2.data);
+              }).catch((err) => {
+                Swal.fire(err.response.data.message);
+              })
+            }).catch(() => {
+              setTokenR(null)
+              navigate("/login");
+            })
         });
     }
-  }, [token, navigate]);
+  }, [tokenR, tokenA, navigate]);
 
   return (
     <>
